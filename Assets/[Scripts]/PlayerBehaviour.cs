94fccbea5b0dd7b0ea89f42a01e86a16cfe08c4a
Shane Holloway <slholloway@mun.ca>
2022-11-24 13:55:42 -0330

    tested on-screen controls on emulator

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -18,6 +18,11 @@ public class PlayerBehaviour : MonoBehaviour
     public Animator animator;
     public PlayerAnimationState playerAnimationState;
 
+    [Header("Controls")] 
+    public Joystick leftStick;
+    [Range(0.1f, 1.0f)]
+    public float verticalThreshold;
+
     private Rigidbody2D rigidbody2D;
 
 
@@ -25,6 +30,7 @@ public class PlayerBehaviour : MonoBehaviour
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        leftStick = (Application.isMobilePlatform) ? GameObject.Find("Left Stick").GetComponent<Joystick>() : null;
     }
 
     // Update is called once per frame
@@ -40,7 +46,7 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Move()
     {
-        var x = Input.GetAxisRaw("Horizontal");
+        var x = Input.GetAxisRaw("Horizontal") + ((Application.isMobilePlatform) ? leftStick.Horizontal : 0.0f);
 
         if (x != 0.0f)
         {
@@ -63,9 +69,9 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Jump()
     {
-        var y = Input.GetAxis("Jump");
+        var y = Input.GetAxis("Jump") + ((Application.isMobilePlatform) ? leftStick.Vertical : 0.0f);
 
-        if (isGrounded && y > 0.0f)
+        if (isGrounded && y > verticalThreshold)
         {
             rigidbody2D.AddForce(Vector2.up * verticalForce, ForceMode2D.Impulse);
         }
